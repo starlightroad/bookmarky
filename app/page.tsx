1930009bfb9f5035d5e3705afc237f27a920f654
starlightroad <ortizge92@gmail.com>
2024-03-28 23:29:53 -0400

    Add dashboard button to home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,16 @@ import Footer from "@/app/ui/footer";
 import Features from "@/app/ui/features";
 import { TypographyH1, TypographyP } from "@/app/ui/typography";
 import { fetchExternalLinks } from "@/app/lib/data";
+import { auth } from "@/auth";
 
-export default function Home() {
+export default async function Home() {
   const externalLinks = fetchExternalLinks();
+  const session = await auth();
+  const isSignedIn = !!session?.user;
 
   return (
     <>
-      <header className="h-14 w-full border-b px-5 py-3 sm:flex sm:items-center">
+      <header className="fixed h-14 w-full border-b bg-slate-800 px-5 py-3 sm:flex sm:items-center">
         <Container>
           <nav>
             <ul className="flex items-center justify-between">
@@ -22,8 +25,12 @@ export default function Home() {
                 </Link>
               </li>
               <li>
-                <Button size="sm" asChild>
-                  <Link href="/auth/login">Login</Link>
+                <Button className="h-8" size="sm" asChild>
+                  {isSignedIn ? (
+                    <Link href="/dashboard">Go to dashboard</Link>
+                  ) : (
+                    <Link href="/auth/login">Login</Link>
+                  )}
                 </Button>
               </li>
             </ul>
@@ -32,7 +39,7 @@ export default function Home() {
       </header>
 
       <Container>
-        <main className="flex flex-col items-center pt-24">
+        <main className="flex flex-col items-center pt-[152px]">
           <div className="mb-8 max-w-screen-md text-center">
             <header className="px-5">
               <TypographyH1>Your Bookmarks Stored in the Cloud</TypographyH1>
